0da96db4b4048ea6905082d5e30e62e9ec9561b1
rohi0307 <rohitchavan8180@gmail.com>
2025-07-31 12:39:01 +0530

    Second Commit

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,9 +1,16 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+
 import utils.DriverFactory;
 
 public class Base {
-    public static WebDriver driver= DriverFactory.getDriver();
+	public static WebDriver driver = DriverFactory.getDriver();
 
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		driver.close();
+	}
 }
